50bae22777166adab8aca14872c13c5279331278
Denis <dmarkitanov@gmail.com>
2020-01-13 10:03:05 +0600

    trying arxiv API

--- app/index.js
+++ app/index.js
@@ -1,1 +1,6 @@
-console.log('hi')
+const axios = require('axios')
+
+axios.get('http://export.arxiv.org/api/query?search_query=all:electron')
+  .then(response => {
+    console.log(response.data)
+  })
