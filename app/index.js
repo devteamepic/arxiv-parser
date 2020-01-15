209661290abd4d6c968ae3457416992a40fede89
Denis <dmarkitanov@gmail.com>
2020-01-15 19:00:27 +0600

    downloads prettified data

--- app/index.js
+++ app/index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const axios = require('axios')
 const converter = require('xml-js')
+const fs = require('fs')
+const request = require('request')
+
 var rawData = null
 
 const app = express()
 const port = 3000
+const separators = [' ', '\\+', '-', '\\(', '\\)', '\\*', '/', ':', '\\?']
+
+/**
+ * @param {string} str Incoming document title name to prettify to CamelCaseStandard
+ */
+const prettifyFileName = (str) => {
+  var splittedString = str.toLowerCase().split(new RegExp(separators.join('|'), 'g'))
+  for (var i = 0; i < splittedString.length; i++) {
+    splittedString[i] = splittedString[i].charAt(0).toUpperCase() + splittedString[i].substring(1)
+  }
+
+  return splittedString.join('') + '.pdf'
+}
 
 app.listen(port, () => {
   console.log('app listens on port 3000')
@@ -19,7 +35,7 @@ axios.get('http://export.arxiv.org/api/query?search_query=all:electron')
       authors.push({ name: rawData.entry[0].author[i].name._text })
     }
 
-    var output = {
+    var singleWork = {
       id: rawData.entry[0].id._text,
       lastUpdatedDate: rawData.entry[0].updated._text,
       publishedDate: rawData.entry[0].published._text,
@@ -30,36 +46,32 @@ axios.get('http://export.arxiv.org/api/query?search_query=all:electron')
       downloadLink: rawData.entry[0].link[2]._attributes.href + '.pdf'
     }
 
-    console.log(output)
+    singleWork.title = prettifyFileName(singleWork.title)
 
-    // console.log(rawData.entry[0]['arxiv:comment'])
-
-    // downloadLink = rawData.entry[0].link[2]._attributes.href
-
-    // const file = fs.createWriteStream('file.pdf')
-    // new Promise((resolve, reject) => {
-    //   request({
-    //     uri: downloadLink + '.pdf',
-    //     headers: {
-    //       'Accept-Encoding': 'gzip, deflate, br',
-    //       'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
-    //       'Cache-Control': 'max-age=0',
-    //       'Upgrade-Insecure-Requests': '1',
-    //       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
-    //     },
-    //     /* GZIP true for most of the websites now, disable it if you don't need it */
-    //     gzip: true
-    //   })
-    //     .pipe(file)
-    //     .on('finish', () => {
-    //       console.log('The file is finished downloading.')
-    //       resolve()
-    //     })
-    //     .on('error', (error) => {
-    //       reject(error)
-    //     })
-    // })
-    //   .catch(error => {
-    //     console.log(`Something happened: ${error}`)
-    //   })
+    const file = fs.createWriteStream(singleWork.title)
+    new Promise((resolve, reject) => {
+      request({
+        uri: singleWork.downloadLink,
+        headers: {
+          'Accept-Encoding': 'gzip, deflate, br',
+          'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+          'Cache-Control': 'max-age=0',
+          'Upgrade-Insecure-Requests': '1',
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+        },
+        /* GZIP true for most of the websites now, disable it if you don't need it */
+        gzip: true
+      })
+        .pipe(file)
+        .on('finish', () => {
+          console.log('The file is finished downloading.')
+          resolve()
+        })
+        .on('error', (error) => {
+          reject(error)
+        })
+    })
+      .catch(error => {
+        console.log(`Something happened: ${error}`)
+      })
   })
