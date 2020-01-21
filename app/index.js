59469b5a4198758a545a03e79a321447b4a14527
Denis <dmarkitanov@gmail.com>
2020-01-21 15:32:33 +0600

    HOTFIX: delay

--- app/index.js
+++ app/index.js
@@ -7,12 +7,12 @@ const csvWriter = require('./csvWriter')
 
 var rawData = null
 var downloadLinkHolder = null
-var allData = []
 var url = 'http://export.arxiv.org/api/query?search_query=all:phd&max_results='
 var amountOfData = 10
 var progress = 0
 var chunk = 0
 var fileCounter = 0
+var allData = []
 
 const app = express()
 const port = 3000
@@ -61,76 +61,81 @@ const fetchData = () => {
     .then(response => {
       rawData = converter.xml2js(response.data, { compact: true, spaces: 2 }).feed
 
-      for (var j = 0; j < rawData.entry.length; j++) {
-        for (var k = 0; k < rawData.entry[j].link.length; k++) {
-          if (rawData.entry[j].link[k]._attributes.title === 'pdf') {
-            downloadLinkHolder = rawData.entry[j].link[k]._attributes.href + '.pdf'
+      for (let j = 0; j < rawData.entry.length; j++) {
+        setTimeout(() => {
+          for (var k = 0; k < rawData.entry[j].link.length; k++) {
+            if (rawData.entry[j].link[k]._attributes.title === 'pdf') {
+              downloadLinkHolder = rawData.entry[j].link[k]._attributes.href + '.pdf'
+            }
           }
-        }
 
-        var authors = ''
+          var authors = ''
 
-        if (!rawData.entry[j].author.length) {
-          authors = rawData.entry[j].author.name._text
-        } else {
-          for (var i = 0; i < rawData.entry[j].author.length; i++) {
-            authors += rawData.entry[j].author[i].name._text + ', '
+          if (!rawData.entry[j].author.length) {
+            authors = rawData.entry[j].author.name._text
+          } else {
+            for (var i = 0; i < rawData.entry[j].author.length; i++) {
+              authors += rawData.entry[j].author[i].name._text + ', '
+            }
           }
-        }
 
-        rawData.entry[j].title._text = deleteNewLine(rawData.entry[j].title._text)
-        rawData.entry[j].title._text = prettifyFileName(rawData.entry[j].title._text)
+          rawData.entry[j].title._text = deleteNewLine(rawData.entry[j].title._text)
+          rawData.entry[j].title._text = prettifyFileName(rawData.entry[j].title._text)
 
-        var singleWork = {
-          id: rawData.entry[j].id._text,
-          lastUpdatedDate: rawData.entry[j].updated._text,
-          publishedDate: rawData.entry[j].published._text,
-          title: rawData.entry[j].title._text,
-          summary: rawData.entry[j].summary._text,
-          authors: authors,
-          downloadLink: downloadLinkHolder,
-          filePath: './files/' + rawData.entry[j].title._text
-        }
+          var singleWork = {
+            id: rawData.entry[j].id._text,
+            lastUpdatedDate: rawData.entry[j].updated._text,
+            publishedDate: rawData.entry[j].published._text,
+            title: rawData.entry[j].title._text,
+            summary: rawData.entry[j].summary._text,
+            authors: authors,
+            downloadLink: downloadLinkHolder,
+            filePath: './files/' + rawData.entry[j].title._text
+          }
 
-        singleWork.summary = deleteNewLine(singleWork.summary)
+          singleWork.summary = deleteNewLine(singleWork.summary)
 
-        allData.push(singleWork)
+          allData.push(singleWork)
 
-        const file = fs.createWriteStream('./files/' + singleWork.title)
-        console.log('Downloading: ' + singleWork.title)
-        new Promise((resolve, reject) => {
-          request({
-            uri: singleWork.downloadLink,
-            headers: {
-              'Accept-Encoding': 'gzip, deflate, br',
-              'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
-              'Cache-Control': 'max-age=0',
-              'Upgrade-Insecure-Requests': '1',
-              'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
-            },
-            /* GZIP true for most of the websites now, disable it if you don't need it */
-            gzip: true
-          })
-            .pipe(file)
-            .on('finish', () => {
-              progress += chunk
-              fileCounter++
-              process.stdout.write('\r\x1b[K')
-              process.stdout.write('Progress: ' + progress + '% Files:' + fileCounter + ' / ' + amountOfData)
-              resolve()
+          const file = fs.createWriteStream('./files/' + singleWork.title)
+          // console.log('Downloading: ' + singleWork.title)
+          new Promise((resolve, reject) => {
+            request({
+              uri: singleWork.downloadLink,
+              headers: {
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+                'Cache-Control': 'max-age=0',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+              },
+              /* GZIP true for most of the websites now, disable it if you don't need it */
+              gzip: true
             })
-            .on('error', (error) => {
-              reject(error)
-            })
-        })
-          .catch(error => {
-            console.log(`Something happened: ${error}`)
+              .pipe(file)
+              .on('finish', () => {
+                progress += chunk
+                fileCounter++
+                process.stdout.write('\r\x1b[K')
+                process.stdout.write('Progress: ' + progress + '% Files: ' + fileCounter + ' / ' + amountOfData)
+                if (fileCounter == amountOfData) {
+                  csvWriter
+                    .writeRecords(allData)
+                    .then(() => {
+                      console.log('\n data added to data.csv, Done!')
+                      resolve()
+                    })
+                }
+                resolve()
+              })
+              .on('error', (error) => {
+                reject(error)
+              })
           })
+            .catch(error => {
+              console.log(`Something happened: ${error}`)
+            })
+        }, j * 1000)
       }
-      csvWriter
-        .writeRecords(allData)
-        .then(() => {
-          console.log('data added to data.csv')
-        })
     })
 }
